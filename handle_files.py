f9143f39343255a1af156cdcfda347dac6d9221c
Timothy Kirk <46324081+tkirk70@users.noreply.github.com>
2024-05-03 10:20:41 -0400

    Update handle_files.py

--- handle_files.py
+++ handle_files.py
@@ -11,8 +11,7 @@ st.caption("""
          \n2. JSON
          \n3. XML
          \n4. XLSX
-         \n**You can upload multiple files.**
-         \nYou can upload mulitple files.""")
+         \n**You can upload multiple files.**""")
 
 uploaded_files = st.file_uploader("Choose a file", accept_multiple_files=True)
 
